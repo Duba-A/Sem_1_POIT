df1bbdaed277330be9efd1a0f34a2333d970b1c4
Duba <63540600+Duba-A@users.noreply.github.com>
2023-09-29 16:15:27 +0300

    Add files via upload

---
+++ Labs OAIP/Lab_04/Lab_04_11_1/Lab_04_11_1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_CTYPE, "Russian");
+    char Char; // объявляем переменную
+    cout << "Введите символ для заполнения: ";
+    cin >> Char; // вводим символ
+
+    for (int i = 0; i < 5; i++) {
+       
+        cout << endl;                        // Рисуем квадрат с помощью цикла for
+
+        for (int j = 0; j < 10; j++) {
+            cout << Char;      
+
+        } 
+    }
+}
---
+++ Labs OAIP/Lab_04/Lab_04_9_1/Lab_04_3_1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>;
+using namespace std;
+void main() {
+    setlocale(LC_CTYPE, "Russian");
+    char Char; // объявляем переменную
+    cout << "Введите символ для заполнения: ";
+    cin >> Char; // вводим символ
+
+    cout << " " << " " << " " << " " << Char << " " << endl;
+    cout << " " << " " << " " << Char << Char << Char << " " << endl;
+    cout << " " << " " << Char << Char << Char << Char << Char << " " << endl;            // рисуем ёлочку
+    cout << " " << Char << Char << Char << Char << Char << Char << Char << " " << endl;
+    cout << Char << Char << Char << Char << Char << Char << Char << Char << Char << endl;
+    cout << " " << " " << " " << " " << Char << " " << " " << " " << " " << endl;
+   
+}
