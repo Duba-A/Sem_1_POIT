cb8e24c14c3dbeb8f386cbb3fe4fe4497c0ab255
Duba <63540600+Duba-A@users.noreply.github.com>
2023-10-04 00:22:33 +0300

    Add files via upload

---
+++ Labs OAIP/Lab_05/Lab_05_D_4/Lab_05_D_4.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    setlocale(LC_CTYPE, "Russian");
+    double r, p, q;
+    char c1, c2, c3;
+    cout << "Введите радиус шара: ";
+    cin >> r;
+    cout << "Введите диагонали ромбообразного отверстия в формате (p, q): ";
+    cin >> c1 >> p >> c2 >> q >> c3;
+    if (c1 != '(' || c2 != ',' || c3 != ')' || p < 0 || q < 0) {
+        cout << "Ошибка ввода диагоналей ромбообразного отверстия" << endl;
+        return 0;
+    }
+    double d = 2 * r;
+    if (d <= p && d <= q) {
+        cout << "Шар пройдет через ромбообразное отверстие" << endl;
+    }
+    else {
+        cout << "Шар не пройдет через ромбообразное отверстие" << endl;
+    }
+    return 0;
+}
---
+++ Labs OAIP/Lab_05/Lab_05_D_5/Lab_05_D_5.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_CTYPE, "Russian");
+    int a, b, c, r, s, t;
+    char c1, c2, c3, c4;
+    cout << "Введите размеры коробки в формате (a, b, c): ";
+    cin >> c1 >> a >> c2 >> b >> c3  >> c >> c4;
+    if (c1 != '(' || c2 != ',' || c3 != ',' || c4 != ')' || a < 1 || b < 1 || c < 1) {
+        cout << "Ошибка ввода размеров коробки" << endl;
+        return 0;
+    }
+    cout << "Введите размеры посылки в формате (r, s, t): ";
+    cin >> c1 >> r >> c2 >> s >> c3 >> t >> c4;
+    if (c1 != '(' || c2 != ',' || c3 != ',' || c4 != ')' || r < 1 || s < 1 || t < 1) {
+        cout << "Ошибка ввода размеров посылки" << endl;
+        return 0;
+    }
+    if (a <= r && b <= s && c <= t || a <= r && b <= t && c <= s || a <= s && b <= r && c <= t || a <= s && b <= t && c <= r || a <= t && b <= r && c <= s || a <= t && b <= s && c <= r) {
+        cout << "Коробку можно упаковать в посылку" << endl;
+    }
+    else {
+        cout << "Коробку нельзя упаковать в посылку" << endl;
+    }
+    return 0;
+}
