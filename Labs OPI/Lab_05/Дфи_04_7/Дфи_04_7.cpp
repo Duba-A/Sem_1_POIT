f431e5505eb738e3916f30bc9fa7474ef0ced59f
Duba <63540600+Duba-A@users.noreply.github.com>
2023-12-01 14:52:31 +0300

    Add files via upload

---
+++ Labs OPI/Lab_05/Дфи_04_7/Дфи_04_7.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_CTYPE, "Russian");
+    double X, Y, Z, Q;
+
+    cout << "Введите количество денег (X): ";
+    cin >> X;
+
+    cout << "Введите стоимость хлеба (Y): ";
+    cin >> Y;
+
+    cout << "Введите стоимость худи (Z): ";
+    cin >> Z;
+
+    cout << "Введите стоимость ручки (Q): ";
+    cin >> Q;
+
+    if (X > Y) {
+        X = X - Y;
+
+        if (X > Z) {
+            X = X - Z;
+
+            if (X > Q) {
+                X = X - Q;
+                cout << "Вы всё купили!" << endl;
+            }
+            else {
+                cout << "Недостаточно средств." << endl;
+            }
+        }
+        else {
+            cout << "Недостаточно средств." << endl;
+        }
+    }
+    else {
+        cout << "Недостаточно средств." << endl;
+    }
+
+    return 0;
+}
